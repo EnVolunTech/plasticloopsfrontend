e0cfd4325f897b65273cba30b46beee91fe8d09d
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-07-16 19:47:09 +0200

    add barter form textarea

--- components/BarterForm/BarterForm.tsx
+++ components/BarterForm/BarterForm.tsx
@@ -1,12 +1,67 @@
+import { ItemPostProps } from "@components/common/ItemPost/ItemPost";
 import s from "./BarterForm.module.css";
+import Image from "next/image";
+import { Avatar } from "@components/common";
+import cn from "classnames";
+import Link from "next/link";
 
-interface Props {}
+export interface BarterFormProps extends ItemPostProps {}
 
-const BarterForm = (props: Props) => {
+const BarterForm = ({
+  id,
+  name,
+  initials,
+  imageUrl,
+  caption,
+  tags,
+}: BarterFormProps) => {
   return (
-    <div>
-      This is for bartering
-      <div className={s.sample}></div>
+    <div className={s.root}>
+      <div className={s.rootIncoming}>
+        <div className={cn(s.topInfo)}>
+          <div className="flex items-center mb-2">
+            <Avatar initial={initials} />
+            <div className="ml-2">
+              <div className="block py-0 text-xl">{name}</div>
+            </div>
+          </div>
+
+          <h5 className={s.postText}>{caption}</h5>
+        </div>
+        <div className={s.imageContainer}>
+          <Image
+            className="object-cover"
+            src={imageUrl}
+            alt={imageUrl}
+            height={500}
+            width={800}
+            layout="responsive"
+          />
+        </div>
+        <div className={cn(s.bottomInfo)}>
+          <span className="">
+            {tags?.map((tag, index) => {
+              return (
+                <span className={cn(s.tagPill)} key={`${tag}-${index}`}>
+                  {tag}
+                </span>
+              );
+            })}
+          </span>
+        </div>
+      </div>
+
+      <div className="col-span-2">
+        <label className="text-xl">Message Ka-barter</label>
+        <textarea
+          className="w-full px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+          rows={7}
+        />
+
+        <button className="flex items-center w-full px-4 py-2 my-1 text-left bg-red-400 rounded-md focus:outline-none ">
+          Barter!
+        </button>
+      </div>
     </div>
   );
 };
--- pages/barter/[id].tsx
+++ pages/barter/[id].tsx
@@ -1,39 +1,49 @@
 import BarterForm from "@components/BarterForm";
+import { BarterFormProps } from "@components/BarterForm/BarterForm";
 import Leftbar from "@components/Leftbar";
+import { mockImages } from "mock";
+
 import { useSession } from "next-auth/client";
 
-interface Props {}
-
-const item = (props: Props) => {
+const item = (props: BarterFormProps) => {
   const [session, loading] = useSession();
 
-  if (!session) {
-    return (
-      <div className="flex justify-center">
-        <div className="w-[200px]">
-          <a href="/api/auth/signin">
-            <div className="flex items-center w-full px-4 py-2 my-1 text-center bg-red-400 rounded-md focus:outline-none">
-              Click here to signin...
-            </div>
-          </a>
-        </div>
-      </div>
-    );
-  }
-
+  // if (!session) {
+  //   return (
+  //     <div className="flex justify-center">
+  //       <div className="w-[200px]">
+  //         <a href="/api/auth/signin">
+  //           <div className="flex items-center w-full px-4 py-2 my-1 text-center bg-red-400 rounded-md focus:outline-none">
+  //             Click here to signin...
+  //           </div>
+  //         </a>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   return (
     <div className="flex px-8 md:grid md:grid-cols-4">
       <aside className="sticky hidden h-screen grid-cols-1 top-20 lg:flex">
-        <div className="flex-1 px-2">
+        <div className="flex-1 px-10">
           <Leftbar />
         </div>
       </aside>
 
       <div className="w-full col-span-4 px-4 lg:col-span-3">
-        <BarterForm />
+        <BarterForm {...props} />
       </div>
     </div>
   );
 };
 
 export default item;
+
+export async function getServerSideProps(context) {
+  const data = mockImages.filter((data) => context.params.id === data.id);
+
+  return {
+    props: {
+      ...data[0],
+    },
+  };
+}
