b5d6c77e1fa5a363dd683d88084da4fcc5599da8
Almie Carajay <apcarajay@up.edu.ph>
2021-07-17 00:59:17 +0800

    converted images from .PNG to .png

--- components/Leftbar/Leftbar.tsx
+++ components/Leftbar/Leftbar.tsx
@@ -5,16 +5,20 @@ interface Props {}
 
 const Leftbar = (props: Props) => {
   return (
-    <div className={s.root}>
-      <div className={s.buttonContainer}>
-        <button className={cn(s.button, { "bg-blue-500 bg-opacity-20": true })}>
-          Barter
-        </button>
-        <button className={s.button}>Sell</button>
-        <button className={s.button}>Search</button>
-        <button className={s.button}>Learn</button>
+    <div>
+      <div className={s.root}>
+        <div className={s.buttonContainer}>
+          <button className={cn(s.button, { "bg-blue-500 bg-opacity-20": true })}>
+            Barter
+          </button>
+          <button className={s.button}>Sell</button>
+          <button className={s.button}>Search</button>
+          <button className={s.button}>Learn</button>
+        </div>
       </div>
+      <div></div>
     </div>
+
   );
 };
 
